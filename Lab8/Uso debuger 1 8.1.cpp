a8176b77211e8fcfe62cab44f4c3d34adc9c3f26
G1mmynki4 <gabrybaldo06@gmail.com>
2025-11-18 10:51:02 +0100

    updatwd gitignore

--- Lab8/Uso debuger 1 8.1.cpp
+++ Lab8/Uso debuger 1 8.1.cpp
@@ -1,3 +1,32 @@
-//
-// Created by gabry on 15/11/2025.
-//
+#include <iostream>
+using namespace std;
+
+int f(int a){
+  a *= 2;
+  return a;
+}
+
+int main(){
+  int n;
+  n=2;
+
+  int*p;
+  p = &n;
+  *p = 5;
+
+  int& r=n;
+  r = 9;
+
+  int m = f(n);
+  cout << n << " " << m << endl;
+  int dim;
+  cin >> dim;
+  if(dim <= 0)
+    dim = 3;
+  int* q = new int[dim];
+
+  for(int i = 0; i < dim; ++i)
+    cin >> q[i];
+
+  return 0;
+}
